2d623b8def8b61a0f273f3f81cfb0330d13f10a0
Aryan Dahiya <aryan23dahiya@gmail.com>
2024-01-14 20:30:40 +0530

    final commit before restarting

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -5,3 +5,18 @@ export default function page() {
         <SignIn />
     )
 }
+
+
+// import { SignIn } from "@clerk/nextjs";
+
+// export default async function Page() {
+    // const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+    // await delay(10000);
+
+//     return (
+//         <div>
+//             <SignIn />
+//         </div>
+//     );
+// }
--- components/shared/EventForm.tsx
+++ components/shared/EventForm.tsx
@@ -17,6 +17,8 @@ import { Input } from "@/components/ui/input"
 import { eventFormSchema } from "@/lib/validator"
 import { IEvent } from "@/lib/database/models/event.model"
 import { eventDefaultValues } from "@/constants"
+import Dropdown from "./Dropdown"
+import { Textarea } from "../ui/textarea"
 
 type EventFormProps = {
     userId: string
@@ -36,7 +38,7 @@ const EventForm = ({ userId, type, event, eventId }: EventFormProps) => {
 
     function onSubmit(values: z.infer<typeof eventFormSchema>) {
         console.log(values)
-        // alert(values.title);
+        console.log('hey')
     }
 
     return (
@@ -66,7 +68,7 @@ const EventForm = ({ userId, type, event, eventId }: EventFormProps) => {
                         render={({ field }) => (
                             <FormItem className="w-full">
                                 <FormControl>
-                                    <Input placeholder="Event title" {...field} className="input-field" />
+                                    <Dropdown onChangeHandler={field.onChange} value={field.value} />
                                 </FormControl>
                                 <FormDescription>
                                     This is your public display name.
@@ -76,6 +78,38 @@ const EventForm = ({ userId, type, event, eventId }: EventFormProps) => {
                         )}
                     />
                 </div>
+
+                <div className="flex flex-col gap-5 md:flex-row">
+                    <FormField
+                        control={form.control}
+                        name="description"
+                        render={({ field }) => (
+                            <FormItem className="w-full">
+                                <FormControl className="h-72">
+                                    <Textarea placeholder="Description" {...field} className="textarea rounded-2xl" />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    {/* <FormField
+                        control={form.control}
+                        name="imageUrl"
+                        render={({ field }) => (
+                            <FormItem className="w-full">
+                                <FormControl className="h-72">
+                                    <FileUploader
+                                        onFieldChange={field.onChange}
+                                        imageUrl={field.value}
+                                        setFiles={setFiles}
+                                    />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    /> */}
+                </div>
+
 
                 <Button type="submit">Submit</Button>
             </form>
