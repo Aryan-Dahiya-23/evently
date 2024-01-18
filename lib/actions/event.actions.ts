0cd70d3c2a4f4d7aac9e66d3cf68b00813876fb5
Aryan Dahiya <aryan23dahiya@gmail.com>
2024-01-19 02:55:39 +0530

    project complete

--- lib/actions/event.actions.ts
+++ lib/actions/event.actions.ts
@@ -126,7 +126,7 @@ export async function getAllEvents({ query, limit = 6, page, category }: GetAllE
 }
 
 // GET EVENTS BY ORGANIZER
-export async function getEventsByUser({ userId, limit = 6, page }: GetEventsByUserParams) {
+export async function getEventsByUser({ userId, limit = 3, page }: GetEventsByUserParams) {
     try {
         await connectToDatabase()
 
